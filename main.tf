73e938f8b9938968fe7909ed58c05af7e5317dff
Natan Oliveira <natan.oliveira0010@gmail.com>
2020-08-01 14:53:21 -0300

    increase default node count

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "google_container_cluster" "gke_cluster" {
   location = var.region
 
   remove_default_node_pool = true
-  initial_node_count       = 0
+  initial_node_count       = 1
 
   master_auth {
     username = ""
